729f6255e9e73a1efbb31d63deeaed3bc009ba25
DevAndy <creator.youngjin@gmail.com>
2019-10-24 16:14:10 +0900

    Done Security 설정

--- src/main/java/com/lecturesearch/lecture/user/config/SecurityConfig.java
+++ src/main/java/com/lecturesearch/lecture/user/config/SecurityConfig.java
@@ -1,22 +1,45 @@
 package com.lecturesearch.lecture.user.config;
 
+import com.lecturesearch.lecture.user.SocialType;
 import com.lecturesearch.lecture.user.oauth.ClientResources;
+import com.lecturesearch.lecture.user.oauth.UserTokenService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.CompositeFilter;
+
+import javax.servlet.Filter;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.lecturesearch.lecture.user.SocialType.FACEBOOK;
+import static com.lecturesearch.lecture.user.SocialType.GOOGLE;
+import static com.lecturesearch.lecture.user.SocialType.KAKAO;
+
 
 @Configuration
 @EnableWebSecurity
 @EnableOAuth2Client
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     // WebSecurityConfigurerAdapter = security 최적화설정
+
+    @Autowired
+    private OAuth2ClientContext oAuth2ClientContext;
 
     @Override
     protected void configure(HttpSecurity https) throws Exception {
@@ -41,8 +64,52 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .invalidateHttpSession(true)
             .and()
                 .addFilterBefore(filter, CsrfFilter.class)
+                .addFilterBefore(oauth2Filter(), BasicAuthenticationFilter.class)
                 .csrf().disable();
     }
+
+    @Bean
+    public FilterRegistrationBean oauth2ClientFilterRegistration(
+            OAuth2ClientContextFilter filter
+    ) {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(filter);
+        registration.setOrder(-100);
+        return registration;
+    }
+
+    private Filter oauth2Filter(){
+        CompositeFilter filter = new CompositeFilter();
+        List<Filter> filters = new ArrayList<>();
+        filters.add(oauth2Filter(facebook(), "/login/facebook", FACEBOOK));
+        filters.add(oauth2Filter(google(), "/login/google", GOOGLE));
+        filters.add(oauth2Filter(kakao(), "/login/kakao", KAKAO));
+        filter.setFilters(filters);
+        return filter;
+    }
+
+    private Filter oauth2Filter(
+            ClientResources client,
+            String path,
+            SocialType socialType
+    ) {
+        OAuth2ClientAuthenticationProcessingFilter filter =
+                new OAuth2ClientAuthenticationProcessingFilter(path);
+        OAuth2RestTemplate template = new OAuth2RestTemplate(client.getClient(),
+                oAuth2ClientContext);
+        filter.setRestTemplate(template);
+        filter.setTokenServices(new UserTokenService(client, socialType));
+        filter.setAuthenticationSuccessHandler(
+                (request, response, authentication) -> response.sendRedirect(
+                        "/"+socialType.getValue()+"/complete"
+                )
+        );
+        filter.setAuthenticationFailureHandler(
+                (request, response, exception) -> response.sendRedirect("/error")
+        );
+        return filter;
+    }
+
 
     @Bean
     @ConfigurationProperties("facebook")
---
+++ src/main/java/com/lecturesearch/lecture/user/oauth/UserTokenService.java
@@ -0,0 +1,4 @@
+package com.lecturesearch.lecture.user.oauth;
+
+public class UserTokenService {
+}
